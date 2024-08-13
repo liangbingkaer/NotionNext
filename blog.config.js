0a1819045d6a8c861b5930a713de4078d00f8b5a
liangbingkaer <101870977+liangbingkaer@users.noreply.github.com>
2024-08-14 06:34:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,12 +20,12 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是彭龙,很高兴看见你🎉',
+    '我是彭龙,很高兴看见你',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '彭龙', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '研究生一枚啊', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '研究生一枚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.plxray.cn/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'xray, pulsar', // 网站关键词 英文逗号隔开
 
